7ce8fc0049b83eab38364c679fbc4979db1216f6
karingula <karingula@users.noreply.github.com>
2019-05-01 10:17:39 -0400

    local custom directive

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -4,7 +4,8 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1 v-highlight:background.delayed="'red'"> File a Complaint! </h1>
-                    <h2 v-highlight="'purple'"> custom directive example </h2>
+                    <h2 v-highlight="'purple'"> Custom Global directive example </h2>
+                    <h3 v-local-highlight="'lightcoral'"> Custom Local directive example </h3>
                     <p v-text="'Some sample text'"></p>
                     <p v-html="'<strong> Some strong text </strong>'"></p>
                     <hr>
@@ -172,6 +173,23 @@
         },
         components: {
             mySwitch: Switch
+        },
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode, oldnode) {
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000
+                    }
+                    setTimeout(() => {
+                        if(binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value
+                        } else {
+                            el.style.color = binding.value
+                        }
+                    }, delay)
+                }
+            }
         },
         methods: {
             submitted() {
