e054d582d7cdcca0ebbbe4de9c3bd25ec88e3227
karingula <karingula@users.noreply.github.com>
2019-06-07 09:12:22 -0400

    change the component name as Navigation

---
+++ portfolio/src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+        <b-navbar class="navbar-expand-lg" toggleable="lg" fixed="top" id="mainNav">
+            <div class="container">
+                <b-navbar-brand href="#page-top">Start Bootstrap</b-navbar-brand>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#team">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </b-navbar>
+</template>
+
