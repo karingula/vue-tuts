d56f195f499b9c319098dbe264a336984d519ecb
karingula <karingula@users.noreply.github.com>
2019-06-08 10:28:22 -0400

    header improvements

--- portfolio/src/components/Header.vue
+++ portfolio/src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
     <header>
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-lead-in">Welcome to our Studio</div>
-                <div class="intro-heading text-uppercase"> Nice to Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-            </div>
-        </div>
+        <b-img :src="require('../assets/img/workstation.jpg')" fluid-grow ></b-img>
+        <!-- <div class="intro-text">
+            <div class="intro-lead-in">Welcome to our Studio!</div>
+            <div class="intro-heading text-uppercase"> Nice to Meet You</div>
+            <a class="btn btn-primary btn-m text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+        </div> -->
     </header>
 </template>
