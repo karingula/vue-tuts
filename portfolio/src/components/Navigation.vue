9fe92aeaf50bb33d109d1b5ed9c595bc8b5bb4e3
karingula <karingula@users.noreply.github.com>
2019-06-14 12:37:11 -0400

    adjustments in nav link items

--- portfolio/src/components/Navigation.vue
+++ portfolio/src/components/Navigation.vue
@@ -1,19 +1,21 @@
 <template>
     <b-navbar class="navbar-section" fixed="top">
         <b-navbar-brand class=brand href="#page-top">V</b-navbar-brand>
-        <ul class="nav-links ml-auto">
-            <li class="nav-item">
-                <a href="#home"> Home </a>
-            </li>
-            <li class="nav-item">
-                <a href="#services"> Services </a>
-            </li>
-            <li class="nav-item">
-                <a href="#about"> About </a>
-            </li>
-            <li class="nav-item">
-                <a href="#contact"> Contact </a>
-            </li>
-        </ul>
+        <b-navbar-nav class="ml-auto nav-section">
+            <ul class="nav-links">
+                <li class="nav-item">
+                    <a href="#home"> Home </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#services"> Services </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#about"> About </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#contact"> Contact </a>
+                </li>
+            </ul>
+        </b-navbar-nav>
     </b-navbar>
 </template>
