73893e8da997a0e092e767d5888f6efe2247443c
karingula <karingula@users.noreply.github.com>
2019-06-20 09:22:53 -0400

    add routes

---
+++ portfolio/src/routes/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Contact from '@/components/Contact'
+
+Vue.use(VueRouter)
+const routes = [
+    {
+        path: '/contact',
+        name: 'Contact Page',
+        component: Contact
+    }
+]
+
+export default new VueRouter({
+    routes
+})
