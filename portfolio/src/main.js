194c7f9badfe7593e3f3ecddb71a9a2ad0254f55
karingula <karingula@users.noreply.github.com>
2019-06-20 09:23:13 -0400

    use vue-router

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,11 +3,12 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import router from './routes'
 import './assets/scss/styles.scss'
 
 Vue.config.productionTip = false
 
-
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
