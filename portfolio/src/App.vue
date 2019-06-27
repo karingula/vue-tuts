b4008d09aa2f251959e65717ff9475ebe79a10a0
karingula <karingula@users.noreply.github.com>
2019-06-27 10:27:48 -0400

    add contact styles and app component changes

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <navigation></navigation>
-    <app-header></app-header>
+    <!-- <app-header></app-header> -->
+    <contact></contact>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+// import Header from './components/Header.vue'
 import Navigation from './components/Navigation.vue'
+import Contact from './components/Contact.vue'
 export default {
   name: 'app',
   components: {
     navigation: Navigation,
-    appHeader: Header
+    // appHeader: Header,
+    contact: Contact
   }
 }
 </script>
