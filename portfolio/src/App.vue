076a88254a513dceebf8037970e0f86334f6dc57
karingula <karingula@users.noreply.github.com>
2019-06-27 10:43:54 -0400

    remove importing contact component. use routing instead via nav-links

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <navigation></navigation>
     <!-- <app-header></app-header> -->
-    <contact></contact>
     <router-view></router-view>
   </div>
 </template>
@@ -10,13 +9,11 @@
 <script>
 // import Header from './components/Header.vue'
 import Navigation from './components/Navigation.vue'
-import Contact from './components/Contact.vue'
 export default {
   name: 'app',
   components: {
     navigation: Navigation,
     // appHeader: Header,
-    contact: Contact
   }
 }
 </script>
