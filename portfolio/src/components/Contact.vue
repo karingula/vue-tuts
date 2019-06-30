d32059486f2f43c553aece07a820c004045b3d3e
karingula <karingula@users.noreply.github.com>
2019-06-30 07:14:56 -0400

    add a wrapper text

--- portfolio/src/components/Contact.vue
+++ portfolio/src/components/Contact.vue
@@ -1,9 +1,18 @@
 <template>
     <div id="container">
         <div id="contact-title">
-            <span>Don't be Stranger</span>
+            <span>Don't be a Stranger</span>
             <br>
-            <span class="greetings">Say Hello</span>
+            <span class="greetings">Say Hello!</span>
+        </div>
+        <div class="wrapper">
+            <div class="col-l" style="margin-bottom: 60px;">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam.
+                </p>
+            </div>
         </div>
     </div>
 </template>
