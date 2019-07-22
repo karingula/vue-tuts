f7f072fe9b76ed44f1fe560a849023400dd71868
karingula <karingula@users.noreply.github.com>
2019-07-22 17:42:31 -0400

    add validation library

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,13 +1,14 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import VeeValidate from "vee-validate";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./routes";
 import "./assets/scss/styles.scss";
 
 Vue.config.productionTip = false;
-
+Vue.use(VeeValidate);
 new Vue({
   router,
   render: h => h(App)
