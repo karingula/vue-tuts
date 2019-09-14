43d3c620462de3ee07ae2eb6a9af02caa4228c8c
Karingula <karingula.v@gmail.com>
2019-09-14 07:15:39 -0400

    register libraries

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -4,9 +4,19 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import "./assets/scss/styles.scss";
+import BootstrapVue from 'bootstrap-vue';
+import VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false;
-
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate, {
+  // This is the default
+  inject: true,
+  // Important to name this something other than 'fields'
+  fieldsBagName: "veeFields",
+  // This is not required but avoids possible naming conflicts
+  errorBagName: "veeErrors"
+});
 new Vue({
   router, //custom router configuration
   store, //custom store implementation
