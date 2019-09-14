79741e17c42fce2744c58ebbb2b468efe17a424d
Karingula <karingula.v@gmail.com>
2019-09-14 07:16:09 -0400

    update router

--- portfolio/src/router.ts
+++ portfolio/src/router.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import Contact from "./components/Contact.vue";
+import Contact from "@/components/Contact.vue";
 
 Vue.use(Router);
 
